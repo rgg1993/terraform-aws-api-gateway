a2b284dcda038001439857d4b1e3c4dab995a8fa
Rosina Garagorry <rosina.garagorry@globant.com>
2022-08-23 16:13:59 -0300

    Add missing descriptions to variables in module

--- modules/apigatewayv2_stage-0.0.1/variables.tf
+++ modules/apigatewayv2_stage-0.0.1/variables.tf
@@ -9,12 +9,36 @@ variable "name" {
 
 
 variable "auto_deploy" {
-
+  description = "Enable autodeploy"
+  type = bool
 }
 
-variable "default_route_settings_detailed_metrics_enabled" {}
-variable "default_route_settings_logging_level" {}
-variable "default_route_settings_throttling_burst_limit" {}
-variable "default_route_settings_throttling_rate_limit" {}
-variable "access_log_settings_destination_arn" {}
-variable "access_log_settings_format" {}
+variable "default_route_settings_detailed_metrics_enabled" { 
+  description = "Enable detailed metrics"
+  type = bool
+}
+
+variable "default_route_settings_logging_level" {
+  description = "Default route settings logging level"
+  type = string
+}
+
+variable "default_route_settings_throttling_burst_limit" {
+  description = "Default route settings throttling burst limit"
+  type = string
+}
+
+variable "default_route_settings_throttling_rate_limit" {
+  description = "Default route settings throttling rate limit"
+  type = string
+}
+
+variable "access_log_settings_destination_arn" {
+  description = "Access log settings destination arn"
+  type = string
+}
+
+variable "access_log_settings_format" {
+  description = "Access log settings format"
+  type = string
+}
